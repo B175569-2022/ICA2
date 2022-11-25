0eaba4d39fb821f62ea369266f23cfc7c4da7880
B175569-2022 <email_not_provided>
2022-11-25 16:58:00 +0000

    added user input about no. of seqs, number of species

--- 01.py
+++ 01.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 # import modules
-import os
-import subprocess
+import os, subprocess, sys, re
 import pandas as pd
 
 # import my functions 
@@ -14,7 +13,10 @@ fun.give_dir()
 # ask user to define the protein family and taxonomic group (function outputs a dictionary)
 prot_taxon = fun.give_prot_taxon()
 
+# pyruvate dehydrogenase
+# ascomycete fungi
 # 
+
 # if user provided a taxon name:
 if (list(prot_taxon.keys())[1] == "taxon_name"):
   print("You want to search for a taxonmic group name:")
@@ -25,14 +27,30 @@ if (list(prot_taxon.keys())[1] == "taxon_name"):
   output  = protein.replace(" ", "_") + "_" + beast.replace(" ", "_") # output name (replace whitespace witj "_")
   esearch = 'esearch -db protein -query ' + '\"' + query + '\"'  # get esearch string ~ maybe give option to change database??
   # get fasta sequences:
+  output_fasta = output + ".fa"                                  # fasta file output name (used later)
   format  = 'fasta'
-  efetch  = 'efetch -format ' + format + " > " + output + ".fa"  # get efetch string 
+  efetch  = 'efetch -format ' + format + " > " + output_fasta    # get efetch string 
   full_search_cmd = esearch + " | " + efetch                     # get full cmd string
   print("Getting fasta sequences ...")
   print("Your esearch | efetch command:\n" + full_search_cmd)
   os.system(full_search_cmd)
-  print("Done. Your fasta sequences are stored in: " + output + ".fa")
-  # maybe more formats ???
+  print("Done. Your fasta sequences are stored in: " + output_fasta)
+  # get accession numbers:
+  output_acc = output + ".acc"                                 # accessions file output name (used later)
+  format  = 'acc'
+  efetch  = 'efetch -format ' + format + " > " + output_acc    # get efetch string 
+  full_search_cmd = esearch + " | " + efetch                   # get full cmd string
+  print("Getting accession numbers of sequences ...")
+  print("Your esearch | efetch command:\n" + full_search_cmd)
+  os.system(full_search_cmd)
+  print("Done. Your sequences's accession numbers are stored in: " + output_acc)
+
+  
+  
+  
+  
+  
+  
 #elif (list(prot_taxon.keys())[1] == "taxonid"):
 #  print("You want to search for a taxonid:"
 #  print("Starting searching NCBI protein database for " + prot_taxon["protein"] + " in " + prot_taxon["taxonid"] + ":")
